53d94d8b9ded5a9496465d63195aac0ab6d49312
Tejesh <ishan.tejesh12@gmail.com>
2020-05-04 15:57:47 +0530

    added new File Test2

---
+++ src/com/git/Test2.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		boolean result=true;
+		String rs = "true";
+		if(result && rs.equals("true"))
+			System.out.println("True");
+		else
+			System.out.println("False");
+	}
+
+}
