d82d392eecff8d8bf89a22a46ee93b9cf6e309e7
Tejesh <ishan.tejesh12@gmail.com>
2020-05-04 16:07:26 +0530

    code changes done

--- src/com/git/Test2.java
+++ src/com/git/Test2.java
@@ -4,11 +4,11 @@ public class Test2 {
 	
 	public static void main(String[] args) {
 		boolean result=true;
-		String rs = "true";
-		if(result && rs.equals("true"))
-			System.out.println("True");
+		String rs = "false";
+		if(result && rs.equals("false"))
+			System.out.println("false");
 		else
-			System.out.println("False");
+			System.out.println("true");
 	}
 
 }
