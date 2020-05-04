e85b730ee50e13271bf6ff922227375babcb879b
Tejesh <ishan.tejesh12@gmail.com>
2020-05-04 15:38:45 +0530

    Test 1...............

---
+++ src/com/git/Test1.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		boolean result=true;
+		if(result)
+			System.out.println("True");
+		else
+			System.out.println("False");
+	}
+
+}
